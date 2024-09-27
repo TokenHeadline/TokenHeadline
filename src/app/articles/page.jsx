681c7101dc468a127b9f3bc4be3f9cb812936fea
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2024-09-27 17:44:38 +0530

    photo up

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -71,41 +71,36 @@ const ArticlesPage = () => {
             href={`/article/${news.slug}`}
             aria-label={`${news.slug}`}
             passHref
-            className=' mx-auto flex flex-col shadow-lg overflow-hidden  hover:shadow-2xl transition-shadow duration-300 border-2 border-black'
+            className='mx-auto flex flex-col md:flex-row shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300 border-2 border-black'
             key={index}
           >
-            <div className='flex'>
-              <div className='relative h-60 w-1/3'>
-                <Image
-                  src={news.featuredImage.url}
-                  alt={news.title}
-                  fill
-                  className='object-cover'
-                />
-              </div>
+            <div className='relative h-60 w-full md:w-1/3'>
+              <Image
+                src={news.featuredImage.url}
+                alt={news.title}
+                fill
+                className='object-cover'
+              />
+            </div>
 
-              <div className='p-6 flex flex-col justify-between w-2/3 relative'>
-                <div
-                  className='bg-orange-500 text-black font-semibold px-2 absolute h-8 w-40
-                top-0 font-2xl items-center text-center'
-                >
-                  {news.category.name.toUpperCase()}
-                </div>
-                <h2 className='text-2xl font-semibold text-gray-800 line-clamp-2 mt-4'>
-                  {news.title}
-                </h2>
-                <p className='text-base text-gray-600 line-clamp-3'>
-                  {news.excerpt.split(' ').slice(0, 65).join(' ') + '...'}
+            <div className='p-6 flex flex-col justify-between w-full md:w-2/3 relative'>
+              <div className='bg-orange-500 text-black font-semibold px-2 absolute h-8 w-40 top-0 font-2xl items-center text-center'>
+                {news.category.name.toUpperCase()}
+              </div>
+              <h2 className='text-2xl font-semibold text-gray-800 line-clamp-2 mt-4'>
+                {news.title}
+              </h2>
+              <p className='text-base text-gray-600 line-clamp-3'>
+                {news.excerpt.split(' ').slice(0, 65).join(' ') + '...'}
+              </p>
+              <div className='flex black'>
+                <p className='text-sm font-normal'>
+                  By {news.author.name.toUpperCase()}
                 </p>
-                <div className='flex  black'>
-                  <p className='text-sm font-normal'>
-                    By {news.author.name.toUpperCase()}
-                  </p>
-                  <span className='mx-2'></span>
-                  <p className='text-sm font-normal'>
-                    {formatDateWithOrdinalAndAbbreviatedMonth(news.date)}
-                  </p>
-                </div>
+                <span className='mx-2'></span>
+                <p className='text-sm font-normal'>
+                  {formatDateWithOrdinalAndAbbreviatedMonth(news.date)}
+                </p>
               </div>
             </div>
           </Link>
@@ -113,34 +108,7 @@ const ArticlesPage = () => {
       </div>
 
       <div className='mt-12 flex justify-center items-center space-x-6 mb-2'>
-        <button
-          className={`px-5 py-2 border-2 transition-all duration-300 text-lg font-medium ${
-            currentPage === 1
-              ? 'border-gray-300 text-gray-300 cursor-not-allowed'
-              : 'border-red-500 text-red-500 hover:bg-red-500 hover:text-white'
-          }`}
-          onClick={previousPage}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </button>
-
-        <span className='text-lg font-semibold text-gray-700'>
-          Page <span className='text-red-600'>{currentPage}</span> of{' '}
-          <span className='text-green-600'>{totalPages}</span>
-        </span>
-
-        <button
-          className={`px-5 py-2  border-2 transition-all duration-300 text-lg font-medium ${
-            currentPage === totalPages
-              ? 'border-gray-300 text-gray-300 cursor-not-allowed'
-              : 'border-green-500 text-green-500 hover:bg-green-500 hover:text-white'
-          }`}
-          onClick={nextPage}
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </button>
+        {/* Pagination Buttons */}
       </div>
     </div>
   )
