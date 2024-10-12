396257439d52dc47ba216083a308f817b1d7681e
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2024-10-12 15:40:12 +0530

    checking the fetching

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -3,9 +3,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://ap-south-1.cdn.hygraph.com/content/cm1adzv1802sy07ut097esnry/master', // Replace with your Hygraph API endpoint
+    uri: 'https://ap-south-1.cdn.hygraph.com/content/cm1adzv1802sy07ut097esnry/master',
   }),
-  cache: new InMemoryCache(),
 })
 
 export default client
