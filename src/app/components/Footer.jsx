b22f206a9941db567dff02278c56fb3989382e9d
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2024-10-14 16:16:26 +0530

    footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -8,13 +8,12 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='bg-blue-300 text-black pt-8 pb-4'>
+    <footer className='bg-stone-950 text-white pt-8 pb-4'>
       <div className='container mx-auto px-4 md:px-12 lg:px-20 xl:px-36 flex flex-col lg:flex-row justify-between items-center md:items-start'>
-        {/* Left Side - Logo and Social Icons */}
         <div className='mb-10 md:mb-0'>
           <Link href='/'>
             <Image
-              src={'/logo.png'}
+              src={'/logowhite.png'}
               width={150}
               height={40}
               alt='logo'
@@ -22,37 +21,9 @@ const Footer = () => {
               className='mb-4'
             />
           </Link>
-          <div className='flex space-x-4 mb-6 justify-center md:justify-start'>
-            <Link
-              href='#'
-              aria-label='Facebook'
-              className='hover:text-gray-600'
-            >
-              <FaFacebook size={40} />
-            </Link>
-            <Link
-              href='https://www.instagram.com/tokenheadline/'
-              aria-label='Instagram'
-              className='hover:text-gray-400 rounded-full'
-              target='_blank'
-            >
-              <FaSquareInstagram size={40} />
-            </Link>
-            <Link
-              href='https://x.com/TokenHeadline/'
-              target='_blank'
-              aria-label='Twitter'
-              className='hover:text-gray-400'
-            >
-              <AiFillTwitterCircle size={40} />
-            </Link>
 
-            <Link href='#' aria-label='YouTube' className='hover:text-gray-400'>
-              <FaYoutube size={40} />
-            </Link>
-          </div>
-          <p className='text-black text-center md:text-left'>
-            &copy; 2024 TokenHeadline
+          <p className='text-white text-center md:text-left'>
+            Copyright &copy; 2024 TokenHeadline
           </p>
         </div>
 
@@ -64,11 +35,7 @@ const Footer = () => {
                   Home
                 </Link>
               </li>
-              <li className='mb-2'>
-                <Link href='opinion' className='hover:text-gray-400'>
-                  Opinions
-                </Link>
-              </li>
+
               <li className='mb-2'>
                 <Link href='articles' className='hover:text-gray-400'>
                   Articles
@@ -81,50 +48,8 @@ const Footer = () => {
               </li>
 
               <li className='mb-2'>
-                <Link href='interview' className='hover:text-gray-400'>
-                  Interview
-                </Link>
-              </li>
-              <li className='mb-2'>
                 <Link href='learn' className='hover:text-gray-400'>
                   Learn
-                </Link>
-              </li>
-
-              <li className='mb-2'>
-                <Link href='advertisement' className='hover:text-gray-400'>
-                  Advertisement
-                </Link>
-              </li>
-              <li>
-                <Link href='partners' className='hover:text-gray-400'>
-                  Partners
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* Column 2 */}
-          <div className='mb-6'>
-            <ul>
-              <li className='mb-2'>
-                <Link href='#' className='hover:text-gray-400'>
-                  Privacy
-                </Link>
-              </li>
-              <li className='mb-2'>
-                <Link href='#' className='hover:text-gray-400'>
-                  Terms
-                </Link>
-              </li>
-              <li className='mb-2'>
-                <Link href='#' className='hover:text-gray-400'>
-                  Disclaimer
-                </Link>
-              </li>
-              <li className='mb-2'>
-                <Link href='#' className='hover:text-gray-400'>
-                  Team
                 </Link>
               </li>
             </ul>
@@ -134,13 +59,48 @@ const Footer = () => {
           <div className='mb-6'>
             <ul>
               <li className='mb-2'>
+                <div className='flex space-x-4 mb-6 justify-center md:justify-start'>
+                  <Link
+                    href='#'
+                    aria-label='Facebook'
+                    className='hover:text-gray-600'
+                  >
+                    <FaFacebook size={40} />
+                  </Link>
+                  <Link
+                    href='https://www.instagram.com/tokenheadline/'
+                    aria-label='Instagram'
+                    className='hover:text-gray-400 rounded-full'
+                    target='_blank'
+                  >
+                    <FaSquareInstagram size={40} />
+                  </Link>
+                  <Link
+                    href='https://x.com/TokenHeadline/'
+                    target='_blank'
+                    aria-label='Twitter'
+                    className='hover:text-gray-400'
+                  >
+                    <AiFillTwitterCircle size={40} />
+                  </Link>
+
+                  <Link
+                    href='#'
+                    aria-label='YouTube'
+                    className='hover:text-gray-400'
+                  >
+                    <FaYoutube size={40} />
+                  </Link>
+                </div>
+              </li>
+              <li className='mb-2'>
                 <Link href='#' className='hover:text-gray-400'>
                   Contact
                 </Link>
               </li>
               <li>
                 <Link href='#' className='hover:text-gray-400'>
-                  FAQs
+                  Disclaimer
                 </Link>
               </li>
             </ul>
