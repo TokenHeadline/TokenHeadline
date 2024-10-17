335ed859102da32e40a5947de3fcbc317c8cd985
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2024-10-18 01:12:04 +0530

    Update page.jsx

--- src/app/learn/[slug]/page.jsx
+++ src/app/learn/[slug]/page.jsx
@@ -95,10 +95,7 @@ const CoursePage = async ({ params }) => {
 
   return (
     <div className='container mx-auto max-w-7xl px-4 lg:px-8 py-2 pb-6'>
-      <head>
-        <title>{course.seoTitle}</title>
-        <meta name='description' content={course.metaDescription} />
-      </head>
+    
 
       <div className='flex flex-col lg:flex-row gap-10'>
         <div className='bg-white shadow-lg rounded-lg p-6 flex-1'>
