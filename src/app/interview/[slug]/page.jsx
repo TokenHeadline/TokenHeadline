cfccc041207244c3ff0d47a4b2ac744be92148dd
Sahil Arora <sahil.2723.a@gmail.com>
2024-10-19 13:09:58 +0530

    pages

--- src/app/interview/[slug]/page.jsx
+++ src/app/interview/[slug]/page.jsx
@@ -14,40 +14,49 @@ const Page = ({ params }) => {
   const [articles, setArticles] = useState([])
   const [recentArticles, setRecentArticles] = useState([])
   const [meta, setMeta] = useState({ title: '', description: '' })
+  const [loading, setLoading] = useState(true) // Adding loading state
+  const [error, setError] = useState(null) // Adding error state
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await client.query({
-        query: GET_INTERVIEW,
-        variables: { slug },
-      })
-
-      const fetchedArticles = data.pressResleases
-
-      if (fetchedArticles.length > 0) {
-        setArticles(fetchedArticles)
-        const article = fetchedArticles[0]
-
-        // Set meta tags for SEO
-        setMeta({
-          title: article.seoTitle || article.title,
-          description:
-            article.seoDescription || 'Default description if none is provided',
+      try {
+        const { data } = await client.query({
+          query: GET_INTERVIEW,
+          variables: { slug },
         })
-      }
 
-      // Fetch recent articles
-      const { data: recentData } = await client.query({
-        query: GET_RECENT_ARTICLES,
-        variables: { slug },
-      })
-      setRecentArticles(recentData.articles)
+        const fetchedArticles = data.pressResleases
+
+        if (fetchedArticles.length > 0) {
+          setArticles(fetchedArticles)
+          const article = fetchedArticles[0]
+
+          // Set meta tags for SEO
+          setMeta({
+            title: article.seoTitle || article.title,
+            description:
+              article.seoDescription ||
+              'Default description if none is provided',
+          })
+        }
+
+        // Fetch recent articles
+        const { data: recentData } = await client.query({
+          query: GET_RECENT_ARTICLES,
+          variables: { slug },
+        })
+        setRecentArticles(recentData.articles)
+      } catch (err) {
+        setError(err.message) // Handle errors
+      } finally {
+        setLoading(false) // Set loading to false after fetch
+      }
     }
 
     fetchData()
   }, [slug])
 
-  if (!articles.length) {
+  if (loading) {
     return (
       <div className='flex justify-center items-center h-screen'>
         <p className='text-xl font-semibold text-green-500'>Loading</p>
@@ -55,23 +64,23 @@ const Page = ({ params }) => {
     )
   }
 
+  if (error) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        <p className='text-xl font-semibold text-red-500'>Error: {error}</p>
+      </div>
+    )
+  }
+
   return (
     <div className='container mx-auto px-4 lg:px-0 pt-0 pb-4 max-w-6xl'>
-      <Head>
-        <title>{meta.title}</title>
+      <head>
+        <title>{meta.title || 'Default'}</title>
         <meta name='description' content={meta.description} />
-        <meta property='og:title' content={meta.title} />
-        <meta property='og:description' content={meta.description} />
-        <meta property='og:image' content={articles[0].featuredImage.url} />
-      </Head>
-
+      </head>
       <div className='mx-auto p-6'>
-        {articles.map((article, index) => (
-          <div key={index} className='mb-10'>
-            <head>
-              <title>{article.title}</title>
-              <meta name='description' content={article.metaDescription} />
-            </head>
+        {articles.map((article) => (
+          <div key={article.id} className='mb-10'>
             <div className='items-center text-center'>
               <h1 className='text-5xl font-extrabold text-gray-900 mb-6'>
                 {article.title}
@@ -101,15 +110,16 @@ const Page = ({ params }) => {
                   italic: ({ children }) => (
                     <em className='italic'>{children}</em>
                   ),
-                  img: ({ src, altText }) => (
-                    <Image
-                      src={src}
-                      alt={altText}
-                      width={800}
-                      height={500}
-                      className='rounded-lg my-5'
-                    />
-                  ),
+                  img: ({ src, altText }) =>
+                    src ? (
+                      <Image
+                        src={src}
+                        alt={altText}
+                        width={800}
+                        height={500}
+                        className='rounded-lg my-5'
+                      />
+                    ) : null,
                   ul: ({ children }) => (
                     <ul className='list-disc pl-6 my-4'>{children}</ul>
                   ),
@@ -163,37 +173,23 @@ const Page = ({ params }) => {
       </div>
       <div className='container mx-auto px-4 lg:px-0 pt-0 pb-4 max-w-6xl flex flex-col md:flex-row'>
         <div className='max-w-full md:max-w-2xl mx-5 bg-gray-50 shadow-lg rounded-lg border border-gray-200 p-6 flex-1 mb-4 md:mb-0'>
-          <h2 className='text-3xl font-semibold text-gray-800 mb-6'>
-            Recent Articles
-          </h2>
-          <ul>
-            {recentArticles.map((recentArticle, index) => (
-              <li key={index} className='flex items-center mb-4'>
-                <Image
-                  src={recentArticle.featuredImage.url}
-                  alt={recentArticle.title}
-                  width={100}
-                  height={60}
-                  className='rounded-md mr-4'
-                />
-                <div>
-                  <Link
-                    href={`/article/${recentArticle.slug}`}
-                    className='text-gray-900 hover:text-blue-600 hover:underline'
-                  >
+          <h2 className='text-2xl font-bold mb-4'>Recent Articles</h2>
+          {recentArticles.length > 0 ? (
+            recentArticles.map((recentArticle) => (
+              <div key={recentArticle.id} className='mb-4'>
+                <h3 className='text-xl font-semibold'>
+                  <Link href={`/articles/${recentArticle.slug}`}>
                     {recentArticle.title}
                   </Link>
-                  <span className='text-gray-500 text-xs block mt-1'>
-                    {new Date(recentArticle.date).toLocaleDateString()}
-                  </span>
-                </div>
-              </li>
-            ))}
-          </ul>
+                </h3>
+                <p className='text-gray-500'>{recentArticle.excerpt}</p>
+              </div>
+            ))
+          ) : (
+            <p>No recent articles available.</p>
+          )}
         </div>
-        <div className='flex-none md:w-1/3'>
-          <Cryptowidget />
-        </div>
+        <Cryptowidget />
       </div>
     </div>
   )
