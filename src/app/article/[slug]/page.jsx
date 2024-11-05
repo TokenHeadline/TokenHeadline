2b3c5cfb2b5186fab6395edc3a961506c3c642a7
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-06 03:14:58 +0530

    trying meta desc

--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -57,11 +57,13 @@ const Page = ({ params }) => {
     )
   }
 
-  const title = article.title
-
   return (
     <div className='container mx-auto px-4 lg:px-0 pt-0 pb-4 max-w-6xl'>
       <div className='mx-auto p-6'>
+        <head>
+          <title>{article.seo.title}</title>
+          <meta name='description' content='Description for the adaajkn' />
+        </head>
         <div className='mb-10'>
           <div className='items-center text-center'>
             <h1 className='text-4xl sm:text-5xl font-extrabold text-gray-900 mb-6'>
