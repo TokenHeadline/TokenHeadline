20f17de7c8b8749ffbf47dcdc98793d6c0f2a176
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-06 16:58:34 +0530

    seo

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -65,7 +65,7 @@ const ArticlesPage = () => {
     )
 
   return (
-    <div className='container items-center mx-auto lg:px-14 md:px-12 px-8 h-screen'>
+    <div className='container items-center mx-auto lg:px-14 md:px-12 px-8 lg:h-screen'>
       <div className='grid grid-cols-1 gap-8 items-center'>
         <head>
           <title>Articles</title>
@@ -76,16 +76,16 @@ const ArticlesPage = () => {
         </head>
         {articles.map((news) => (
           <Link
-            href={`/article/${news.slug}`}
-            aria-label={news.title}
+            href={`/article/${news.slug || 'placeholder-slug'}`}
+            aria-label={news.title || 'Placeholder Title'}
             passHref
             className='mx-auto flex flex-col md:flex-row shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300 border-2 border-black'
-            key={news.id}
+            key={news.id || 'placeholder-id'}
           >
             <div className='relative h-60 w-full md:w-1/3'>
               <Image
-                src={news.featuredImage.node.sourceUrl}
-                alt={news.title}
+                src={news.featuredImage?.node?.sourceUrl || '/logo.png'}
+                alt={news.title || 'Placeholder Title'}
                 fill
                 className='object-cover'
               />
@@ -93,25 +93,29 @@ const ArticlesPage = () => {
 
             <div className='p-6 flex flex-col justify-between w-full md:w-2/3 relative'>
               <div className='bg-orange-500 text-black font-semibold px-2 absolute h-8 w-40 top-0 font-2xl items-center text-center'>
-                {news.categories.nodes[0]?.name.toUpperCase()}
+                {news.categories?.nodes[0]?.name?.toUpperCase() || 'CATEGORY'}
               </div>
               <h2 className='text-2xl font-semibold text-gray-800 line-clamp-2 mt-4'>
-                {news.title}
+                {news.title || 'Placeholder Title'}
               </h2>
               <p className='text-base text-gray-600 line-clamp-3'>
                 {news.excerpt
-                  .replace(/<[^>]+>/g, '')
-                  .split(' ')
-                  .slice(0, 65)
-                  .join(' ') + '...'}
+                  ? news.excerpt
+                      .replace(/<[^>]+>/g, '')
+                      .split(' ')
+                      .slice(0, 65)
+                      .join(' ') + '...'
+                  : 'Placeholder excerpt...'}
               </p>
               <div className='flex'>
                 <p className='text-sm font-normal'>
-                  By {news.author.node.name.toUpperCase()}
+                  By {news.author?.node?.name?.toUpperCase() || 'AUTHOR'}
                 </p>
                 <span className='mx-2'></span>
                 <p className='text-sm font-normal'>
-                  {formatDateWithOrdinalAndAbbreviatedMonth(news.date)}
+                  {news.date
+                    ? formatDateWithOrdinalAndAbbreviatedMonth(news.date)
+                    : 'DATE'}
                 </p>
               </div>
             </div>
