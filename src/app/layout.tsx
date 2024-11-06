da0345f4f2665ed01a11b2e9aa44e76d41c7bed4
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-07 00:03:38 +0530

    testing footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inria_Sans } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Today from './components/Today'
 const inriaSans = Inria_Sans({
   subsets: ['latin'],
   weight: ['300', '400', '700'],
@@ -18,10 +17,9 @@ export default function RootLayout({
         className={inriaSans.className + ' h-screen'}
         style={{ backgroundColor: '#e9e9e9' }}
       >
-        <Today />
         {/* <Navbar /> */}
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   )
