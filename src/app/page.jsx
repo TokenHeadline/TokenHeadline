0d3bc0327eefd28868534dae83eac3b7b53eaee6
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-06 22:19:53 +0530

    removing banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,27 +12,28 @@ import PR from './components/PR'
 
 export default function Home() {
   return (
-    <div className='h-max'>
-      <section className='flex flex-col m-4 lg:flex-row lg:space-x-4 lg:ml-16 lg:mr-16 '>
-        <Today />
-        <Articles />
-        {/* <Banner /> */}
-      </section>
-      <BreakinNewsTicker />
-      <section className='space-y-8 m-4 lg:flex-row lg:space-x-4 lg:mt-4 lg:ml-16 lg:mr-16'>
-        <Topics />
-      </section>
-      <section className='space-y-8 m-4 lg:flex-row lg:space-x-4 lg:mt-8 lg:ml-16 lg:mr-16'>
-        <Latest />
-      </section>
+    <div>HEAVEN</div>
+    // <div className='h-max'>
+    //   <section className='flex flex-col m-4 lg:flex-row lg:space-x-4 lg:ml-16 lg:mr-16 '>
+    //     <Today />
+    //     <Articles />
+    //     {/* <Banner /> */}
+    //   </section>
+    //   <BreakinNewsTicker />
+    //   <section className='space-y-8 m-4 lg:flex-row lg:space-x-4 lg:mt-4 lg:ml-16 lg:mr-16'>
+    //     <Topics />
+    //   </section>
+    //   <section className='space-y-8 m-4 lg:flex-row lg:space-x-4 lg:mt-8 lg:ml-16 lg:mr-16'>
+    //     <Latest />
+    //   </section>
 
-      <section className=''>
-        <ArticlesGrid />
-        <PR />
-      </section>
-      <section className='space-y-8 m-4 lg:flex-row lg:space-x-4 lg:mt-8 lg:ml-16 lg:mr-16'>
-        <Courses />
-      </section>
-    </div>
+    //   <section className=''>
+    //     <ArticlesGrid />
+    //     <PR />
+    //   </section>
+    //   <section className='space-y-8 m-4 lg:flex-row lg:space-x-4 lg:mt-8 lg:ml-16 lg:mr-16'>
+    //     <Courses />
+    //   </section>
+    // </div>
   )
 }
