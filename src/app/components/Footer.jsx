a9ddae51cd0fe7a82ac8ea4b5eb75294c0327c72
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-07 00:27:08 +0530

    testing without link

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -11,16 +11,16 @@ const Footer = () => {
     <div className='bg-stone-950 text-white pt-8 pb-4 mt-8 '>
       <div className='container mx-auto px-4 md:px-12 lg:px-20 xl:px-36 flex flex-col lg:flex-row justify-between items-center md:items-start'>
         <div className='mb-10 md:mb-0'>
-          {/* <Link href='/'>
-            <Image
-              src={'/logowhite.png'}
-              width={150}
-              height={40}
-              alt='logo'
-              priority={true}
-              className='mb-4'
-            />
-          </Link> */}
+          {/* <Link href='/'> */}
+          <Image
+            src={'/logowhite.png'}
+            width={150}
+            height={40}
+            alt='logo'
+            priority={true}
+            className='mb-4'
+          />
+          {/* </Link> */}
 
           <p className='text-white text-center md:text-left'>
             Copyright &copy; 2024 TokenHeadline
