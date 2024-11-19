d0a2eac4a291312fda66ac30498a79bce861c364
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-19 23:29:13 +0530

    Padding all 1

--- src/app/article/[slug]/ArticleContent.jsx
+++ src/app/article/[slug]/ArticleContent.jsx
@@ -48,7 +48,7 @@ const ArticleContent = ({ slug }) => {
 
   if (!article) {
     return (
-      <div className='container pt-0 pb-4 max-w-6xl'>
+      <div className='container pt-0 pb-4 max-w-6xl px-1'>
         <p>Article not found</p>
       </div>
     )
@@ -57,7 +57,7 @@ const ArticleContent = ({ slug }) => {
   const sanitizedContent = DOMPurify.sanitize(article.content)
 
   return (
-    <div className='container pt-0 pb-4 max-w-6xl'>
+    <div className='container pt-0 pb-4 max-w-6xl px-1'>
       <div className='p-4 md:p-6'>
         <div className='mb-10'>
           <div className=' md:text-center'>
--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -4,7 +4,7 @@ const Page = ({ params }) => {
   const { slug } = params
 
   return (
-    <div className='container pt-0 pb-4 max-w-6xl'>
+    <div className='container pt-0 pb-4 max-w-6xl px-1'>
       <ArticleContent slug={slug} />
     </div>
   )
