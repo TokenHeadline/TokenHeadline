1dd2a65d688e5cd3b6fa1a54a27c0d83f2b5744d
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-20 14:13:46 +0530

    Link Sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inria_Sans } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Head from 'next/head'
 
 const inriaSans = Inria_Sans({
   subsets: ['latin'],
@@ -14,14 +13,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  // Set the title and headline as dynamic or static values
-  const title = 'Tokenheadline'
-
   return (
     <html lang='en'>
-      <head>
-        <title>{title}</title>
-      </head>
       <body
         className={inriaSans.className + ' h-screen'}
         style={{ backgroundColor: '#e9e9e9' }}
