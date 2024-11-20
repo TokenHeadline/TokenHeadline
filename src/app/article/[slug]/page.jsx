43c7f3394541039f91cb67000f334c3c29358da0
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-20 14:17:50 +0530

    Link Sharing 2

--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ArticleContent from './ArticleContent'
 import { GET_ARTICLE } from '../../../../services/index'
 import client from '../../../lib/apolloClient'
+
 export async function generateMetadata({ params }) {
   const { slug } = params
   const { data: articleData } = await client.query({
     query: GET_ARTICLE,
     variables: { slug },
   })
-  return {
+
+  const metadata = {
     title: articleData.post.title,
     description:
       articleData.post.excerpt
@@ -16,12 +18,46 @@ export async function generateMetadata({ params }) {
         .split(' ')
         .slice(0, 30)
         .join(' ') + '...',
-    image: articleData.post.featuredImage?.node.sourceUrl,
-    url: 'https://tokenheadline.com/article/' + articleData.post.slug,
-    type: 'article',
-    siteName: 'Token Headline',
+    openGraph: {
+      title: articleData.post.title,
+      description:
+        articleData.post.excerpt
+          .replace(/<[^>]+>/g, '')
+          .split(' ')
+          .slice(0, 30)
+          .join(' ') + '...',
+      url: `https://tokenheadline.com/article/${articleData.post.slug}`,
+      type: 'article',
+      siteName: 'Token Headline',
+      images: [
+        {
+          url:
+            articleData.post.featuredImage?.node.sourceUrl ||
+            '/default-image.jpg',
+          width: 1200,
+          height: 630,
+          alt: articleData.post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: articleData.post.title,
+      description:
+        articleData.post.excerpt
+          .replace(/<[^>]+>/g, '')
+          .split(' ')
+          .slice(0, 30)
+          .join(' ') + '...',
+      images: [
+        articleData.post.featuredImage?.node.sourceUrl || '/default-image.jpg',
+      ],
+    },
   }
+
+  return metadata
 }
+
 const Page = ({ params }) => {
   const { slug } = params
 
