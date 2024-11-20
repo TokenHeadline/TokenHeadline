72b32b79daab18dcf2050913f9eede90eca4b0d8
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-20 14:10:26 +0530

    Link Sharing

--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -1,5 +1,27 @@
 import React from 'react'
 import ArticleContent from './ArticleContent'
+import { GET_ARTICLE } from '../../../../services/index'
+import client from '../../../lib/apolloClient'
+export async function generateMetadata({ params }) {
+  const { slug } = params
+  const { data: articleData } = await client.query({
+    query: GET_ARTICLE,
+    variables: { slug },
+  })
+  return {
+    title: articleData.post.title,
+    description:
+      articleData.post.excerpt
+        .replace(/<[^>]+>/g, '')
+        .split(' ')
+        .slice(0, 30)
+        .join(' ') + '...',
+    image: articleData.post.featuredImage?.node.sourceUrl,
+    url: 'https://tokenheadline.com/article/' + articleData.post.slug,
+    type: 'article',
+    siteName: 'Token Headline',
+  }
+}
 const Page = ({ params }) => {
   const { slug } = params
 
