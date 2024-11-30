fc807a8268f9f38c1a2d16e4311dae18c0c4823c
Sahil Arora <sahil.2723.a@gmail.com>
2024-11-30 22:04:37 +0530

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,31 @@
+import server from '../lib/apolloServer'
+import { gql } from '@apollo/client'
+
 export default async function sitemap() {
   const baseUrl = 'https://tokenheadline.com'
 
-  return [
+  // Define the GraphQL query to fetch all articles
+  const ARTICLES_QUERY = gql`
+    query GET {
+      posts(first: 100) {  // Adjust the number based on your API's pagination limit
+        nodes {
+          slug  // Assuming articles have unique slugs
+          updatedAt  // Assuming articles have a timestamp for last update
+        }
+      }
+    }
+  `
+
+  const { data } = await server.query({ query: ARTICLES_QUERY })
+
+  const articleSitemapEntries = data.posts.nodes.map((article) => ({
+    url: `${baseUrl}/articles/${article.slug}`,
+    lastModified: new Date(article.updatedAt).toISOString().split('T')[0],
+    changeFrequency: 'daily',
+    priority: 0.8,
+  }))
+
+  const staticEntries = [
     {
       url: baseUrl,
       lastModified: new Date().toISOString().split('T')[0],
@@ -12,27 +36,33 @@ export default async function sitemap() {
       url: `${baseUrl}/articles`,
       lastModified: new Date().toISOString().split('T')[0],
       changeFrequency: 'daily',
-      priority: 0.8, // Optional: add priority values for other routes
+      priority: 1,
     },
     {
       url: `${baseUrl}/opinion`,
       lastModified: new Date().toISOString().split('T')[0],
       changeFrequency: 'daily',
+      priority: 1,
     },
     {
       url: `${baseUrl}/press-release`,
       lastModified: new Date().toISOString().split('T')[0],
       changeFrequency: 'daily',
+      priority: 1,
     },
     {
       url: `${baseUrl}/interview`,
       lastModified: new Date().toISOString().split('T')[0],
       changeFrequency: 'daily',
+      priority: 1,
     },
     {
       url: `${baseUrl}/learn`,
       lastModified: new Date().toISOString().split('T')[0],
       changeFrequency: 'daily',
+      priority: 1,
     },
   ]
+
+  return [...staticEntries, ...articleSitemapEntries]
 }
